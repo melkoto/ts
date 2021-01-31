57bc21c49b6b92241c32e69e839db2f0489749e3
Movsar Makhmutov <m.movsar@gmail.com>
2021-02-01 02:30:56 +0300

    Sync class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Movsar', age: 31 });
 
-user.save();
+user.events.on('click', () => {
+  console.log('click');
+});
+
+user.events.trigger('click');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,39 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
 export class User {
+  public events: Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
+    // @ts-ignore
     return this.data[propName];
   }
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    axios
-      .get(`http://localhost:3000/users/${id}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
