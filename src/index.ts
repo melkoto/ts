1283def30e85045e2fda72dfbf0398c8784579e8
Movsar Makhmutov <m.movsar@gmail.com>
2021-02-03 01:42:20 +0300

    save method in User class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ id: 3 });
+const user = new User({ id: 3, name: 'Alina', age: 28 });
 
-user.on('change', () => console.log(user));
+user.on('save', () => console.log(user));
 
-user.fetch();
+user.save();
