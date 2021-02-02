043f25543861f77472aefb055adfd11077020657
Movsar Makhmutov <m.movsar@gmail.com>
2021-02-03 01:20:58 +0300

    set in User

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Movsar', age: 31 });
+const user = new User({ name: 'Mansur', age: 27 });
+
+console.log(user.get('name'));
+
+user.on('change', () => console.log('change'));
+user.trigger('click');
+
+user.set({ name: 'Adam' });
